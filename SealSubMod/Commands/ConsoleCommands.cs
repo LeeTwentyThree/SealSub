0c3563cbd502219472d10a88a986607e89c4df59
Nagorrogan <eldritchcarmaker@gmail.com>
2023-07-16 04:40:57 -0500

    Test to get it in game nothing fancy
    lowered the language version to 10 just so I could build it. Still can't build C#11 for some reason gotta look at that later

---
+++ SealSubMod/Commands/ConsoleCommands.cs
@@ -0,0 +1,17 @@
+﻿using Nautilus.Commands;
+using UnityEngine;
+
+namespace SealSubMod.Commands;//I thought it looked nicer with a folder :)
+
+internal class ConsoleCommands
+{
+    [ConsoleCommand("Sealsub")]
+    public static void SealSubCommand(string arg = "")
+    {
+        if (arg != null) ErrorMessage.AddMessage($"Hello there, {arg}!");//worthless. I just thought would be funny
+
+        var obj = Object.Instantiate(Plugin.assets.LoadAsset<GameObject>("Seal Sub"));
+        obj.SetActive(true);
+        obj.transform.position = Player.main.transform.position + (Camera.main.transform.forward * 30);
+    }
+}
--- SealSubMod/Plugin.cs
+++ SealSubMod/Plugin.cs
@@ -1,7 +1,11 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using Nautilus.Handlers;
+using SealSubMod.Commands;
+using System.IO;
 using System.Reflection;
+using UnityEngine;
 
 namespace SealSubMod;
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
@@ -12,6 +16,9 @@ public class Plugin : BaseUnityPlugin
 
     private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
 
+    internal static readonly string AssetsFolder = Path.Combine(Path.GetDirectoryName(Assembly.Location), "Assets");
+    internal static readonly AssetBundle assets = AssetBundle.LoadFromFile(Path.Combine(AssetsFolder, "sealsubassets"));//This feels wrong but I'm using it for testing for now
+
     private void Awake()
     {
         // set project-scoped logger instance
@@ -19,6 +26,9 @@ public class Plugin : BaseUnityPlugin
 
         // register harmony patches, if there are any
         Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+
+        ConsoleCommandsHandler.RegisterConsoleCommands(typeof(ConsoleCommands));
+
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
     }
 }
