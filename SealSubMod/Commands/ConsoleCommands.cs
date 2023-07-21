3c8e94bbe188a88c5cb3e23daa1ab8800b930082
Nagorrogan <eldritchcarmaker@gmail.com>
2023-07-21 04:12:39 -0500

    comma ands

--- SealSubMod/Commands/ConsoleCommands.cs
+++ SealSubMod/Commands/ConsoleCommands.cs
@@ -6,12 +6,39 @@ namespace SealSubMod.Commands;//I thought it looked nicer with a folder :)
 internal class ConsoleCommands
 {
     [ConsoleCommand("Sealsub")]
-    public static void SealSubCommand(string arg = null)
+    public static void SealSubCommand(bool killLights = true, string arg = "", bool debugColliders = false)
     {
-        if (arg != null) ErrorMessage.AddMessage($"Hello there, {arg}!");//worthless. I just thought would be funny
+        ErrorMessage.AddMessage($"Hello there, {(arg != "" ? arg : "bitch")}!");//worthless. I just thought would be funny
 
-        var obj = Object.Instantiate(Plugin.assets.LoadAsset<GameObject>("Seal Sub"));
+        var obj = Object.Instantiate(Plugin.assets.LoadAsset<GameObject>("SealSubPrefab"));
         obj.SetActive(true);
-        obj.transform.position = Player.main.transform.position + (Camera.main.transform.forward * 30);
+        obj.transform.position = Player.main.transform.position + (Camera.main.transform.forward * 50);
+
+        var shader = Shader.Find("MarmosetUBER");
+        obj.GetComponentsInChildren<Renderer>(true).ForEach(rend => rend.materials.ForEach(mat => mat.shader = shader));
+
+        if(debugColliders) obj.GetComponentsInChildren<Collider>(true).ForEach(candy => ErrorMessage.AddMessage($"Found candy {candy.name}"));
+
+        if (killLights) obj.GetComponentsInChildren<Light>(true).ForEach(ligd => GameObject.Destroy(ligd));
+    }
+    [ConsoleCommand("SetWalking")]
+    public static void SetWalking(bool walking = true)
+    {
+        if (!Player.main)
+        {
+            ErrorMessage.AddMessage("Player.main is null when trying to set walking!!!!!");
+            ErrorMessage.AddMessage("(don't use command when not in game)");
+            ErrorMessage.AddMessage("((if you are in game this is a very confusing situation))");
+            return;
+        }
+        Player.main.precursorOutOfWater = walking;
+    }
+    [ConsoleCommand("W")]
+    public static void WarpForwardShortcut(float distance, bool setWalk = false)
+    {
+        Transform aimingTransform = Player.main.camRoot.GetAimingTransform();
+        Player.main.SetPosition(Player.main.transform.position + aimingTransform.forward * distance);
+        Player.main.OnPlayerPositionCheat();
+        Player.main.precursorOutOfWater = setWalk;
     }
 }
