6c8a468d64051e9ff10efea5137162448972ae0c
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-24 01:08:00 -0400

    PrefabSlot component for tests & maybe upgrade fab

---
+++ SealSubMod/MonoBehaviours/PrefabSlot.cs
@@ -0,0 +1,37 @@
+﻿using SealSubMod.Interfaces;
+using UWE;
+
+namespace SealSubMod.MonoBehaviours;
+
+internal class PrefabSlot : MonoBehaviour, IAsyncPrefabSetupOperation
+{
+    [Header("Whether the Class ID or TechType is used:")]
+    [SerializeField] Mode mode;
+    [Header("The Class ID or TechType to use:")]
+    [SerializeField] string classId;
+    [SerializeField] TechType techType;
+
+    public IEnumerator SetupPrefabAsync()
+    {
+        GameObject prefab;
+        if (mode == Mode.ClassId)
+        {
+            var task = PrefabDatabase.GetPrefabAsync(classId);
+            yield return task;
+            task.TryGetPrefab(out prefab);
+        }
+        else
+        {
+            var task = CraftData.GetPrefabForTechTypeAsync(techType);
+            yield return task;
+            prefab = task.GetResult();
+        }
+        Instantiate(prefab, transform, false);
+    }
+
+    public enum Mode
+    {
+        ClassId,
+        TechType
+    }
+}
