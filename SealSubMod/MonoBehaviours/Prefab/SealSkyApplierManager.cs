5eb755e4f12302339797713f2b83826487ca7944
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-25 00:05:49 -0400

    Add SealSkyApplierManager for proper lighting

--- SealSubMod/MonoBehaviours/Prefab/SealSkyApplierManager.cs
+++ SealSubMod/MonoBehaviours/Prefab/SealSkyApplierManager.cs
@@ -1,12 +1,19 @@
-﻿using SealSubMod.Interfaces;
+﻿using mset;
+using SealSubMod.Interfaces;
+using SealSubMod.Utility;
 
 namespace SealSubMod.MonoBehaviours.Prefab;
 
-internal class SealSkyApplierManager : MonoBehaviour, IOnAsyncPrefabTasksCompleted
+internal class SealSkyApplierManager : MonoBehaviour, IAsyncPrefabSetupOperation, IOnAsyncPrefabTasksCompleted
 {
     public SkyApplier exteriorSkyApplier;
     public SkyApplier interiorSkyApplier;
     public SkyApplier windowSkyApplier;
+
+    public LightingController lightingController;
+
+    [HideInInspector] public Sky skyBaseGlass;
+    [HideInInspector] public Sky skyBaseInterior;
 
     private List<Renderer> _interiorRenderers = new List<Renderer>();
     private List<Renderer> _exteriorRenderers = new List<Renderer>();
@@ -36,5 +43,19 @@ internal class SealSkyApplierManager : MonoBehaviour, IOnAsyncPrefabTasksComplet
         exteriorSkyApplier.renderers = _exteriorRenderers.ToArray();
         interiorSkyApplier.renderers = _interiorRenderers.ToArray();
         windowSkyApplier.renderers = _windowRenderers.ToArray();
+    }
+
+    public IEnumerator SetupPrefabAsync()
+    {
+        yield return CyclopsReferenceManager.EnsureCyclopsReferenceExists();
+        var cyclops = CyclopsReferenceManager.CyclopsReference;
+        skyBaseGlass = Instantiate(cyclops.transform.Find("SkyBaseGlass"), transform).GetComponent<Sky>();
+        skyBaseInterior = Instantiate(cyclops.transform.Find("SkyBaseInterior"), transform).GetComponent<Sky>();
+        lightingController.skies[0].sky = skyBaseGlass;
+        lightingController.skies[1].sky = skyBaseInterior;
+
+        var sub = GetComponent<SealSubRoot>();
+        sub.interiorSky = skyBaseInterior;
+        sub.glassSky = skyBaseGlass;
     }
 }
