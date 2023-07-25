16f33942fd61ffb8a641b9b252fb429afd5931af
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-24 23:24:07 -0400

    New class to get a Cyclops reference at any time

---
+++ SealSubMod/Utility/CyclopsReferenceManager.cs
@@ -0,0 +1,31 @@
+﻿namespace SealSubMod.Utility;
+
+internal static class CyclopsReferenceManager
+{
+    public static GameObject CyclopsReference { get; private set; }
+
+    private static bool _loaded;
+
+    // call this whenever you are about to access CyclopsReference, thanks and goodbye
+    public static IEnumerator EnsureCyclopsReferenceExists()
+    {
+        if (CyclopsReference != null)
+        {
+            yield break;
+        }
+
+        _loaded = false;
+
+        yield return new WaitUntil(() => LightmappedPrefabs.main);
+
+        LightmappedPrefabs.main.RequestScenePrefab("Cyclops", new LightmappedPrefabs.OnPrefabLoaded(OnSubPrefabLoaded));
+
+        yield return new WaitUntil(() => _loaded);
+    }
+
+    private static void OnSubPrefabLoaded(GameObject prefab)
+    {
+        CyclopsReference = prefab;
+        _loaded = true;
+    }
+}
