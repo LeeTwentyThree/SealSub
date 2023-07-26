3ce91f7aa45d3202469bc6cc03945ca6856c6865
Nagorrogan <eldritchcarmaker@gmail.com>
2023-07-26 04:21:47 -0500

    Add undocking funtionality

--- SealSubMod/MonoBehaviours/SealDockingBay.cs
+++ SealSubMod/MonoBehaviours/SealDockingBay.cs
@@ -3,10 +3,36 @@
 internal class SealDockingBay : VehicleDockingBay
 {
     //Mostly a marker class, to distinguish between normal docking bays and the seal bay in patches
-    public static float MaxDistance = 2;
+    public static float MaxDistance = 3;
     public static float MinForce = 0.001f;
-    public static float MaxForce = 3;
+    public static float MaxForce = 4;
     public static ForceMode ForceMode = ForceMode.VelocityChange;
+
+    public static float dockPushOutForce = 2;
+    public static float dockPullInForceMult = 0.01f;
+    public static float dotProductLimit = 0.9f;
+
+
+    public static bool debug = true;//useful
+    public static bool doubleDebug = false;//sometimes useful but very screen spammy
+
+    public void FixedUpdate()
+    {
+        if (!dockedVehicle) return;
+        if(!dockedVehicle.CanPilot() || !dockedVehicle.GetPilotingMode()) return;
+
+        var outDirection = exit.position - dockedVehicle.transform.position;
+        var dot = Vector3.Dot((dockedVehicle.transform.rotation * GameInput.GetMoveDirection()).normalized, outDirection.normalized);
+
+        if(doubleDebug)ErrorMessage.AddMessage($"Dot product {dot}");
+
+        if (dot >= dotProductLimit)
+        {
+            if(debug) ErrorMessage.AddMessage("Trying to undock!!!!!!!!!!!!!!");
+            dockedVehicle.useRigidbody.AddForce(outDirection * dockPushOutForce);
+            OnUndockingComplete(Player.main);
+        }
+    }
 
     private new void LateUpdate()
     {
@@ -38,8 +64,9 @@ internal class SealDockingBay : VehicleDockingBay
         var direction = dockPos - vehclPos;
         var distance = direction.magnitude;
 
-        var interp = (distance / MaxDistance) * (interpolationfractionnumbervalue == 1 ? 1 : 0.25f);//1 means it's fully docked, so if it's not fully docked it's still getting pulled in
-                                                                                                    //and it was way too fast at getting pulled in, so slow it down
+        var interp = distance / MaxDistance;
+        if (interpolationfractionnumbervalue != 1)//1 means it's fully docked, so if it's not fully docked it's still getting pulled in
+            interp *= dockPullInForceMult;       //and it was way too fast at getting pulled in, so slow it down
 
         var force = Mathf.Lerp(MinForce, MaxForce, interp) * direction;
 
