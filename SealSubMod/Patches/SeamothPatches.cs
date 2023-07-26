41707244cc63a19b384255e03f15efd6ad8c1e22
Nagorrogan <eldritchcarmaker@gmail.com>
2023-07-26 06:32:00 -0500

    Fix seamoth animation

---
+++ SealSubMod/Patches/SeamothPatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using SealSubMod.MonoBehaviours;
+
+namespace SealSubMod.Patches;
+
+[HarmonyPatch(typeof(SeaMoth))]
+internal class SeamothPatches
+{
+    [HarmonyPatch(nameof(SeaMoth.UpdateDockedAnim))]
+    public static bool Prefix(SeaMoth __instance)
+    {
+        if (!__instance.docked) 
+            return true;
+
+        if (Player.main.currentMountedVehicle != __instance)
+            return true;
+
+        if (!__instance.GetComponentInParent<SealSubRoot>())//maybe remove this check later. Seems unnecessary?
+            return true;
+
+        __instance.animator.SetBool("docked", false);
+        return false;
+    }
+}
