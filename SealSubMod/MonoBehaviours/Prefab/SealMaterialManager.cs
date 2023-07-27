45842e3dc00e72ef4ba870d63075390020a1cc9a
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-26 21:19:53 -0400

    Tone down reflections a *little*

--- SealSubMod/MonoBehaviours/Prefab/SealMaterialManager.cs
+++ SealSubMod/MonoBehaviours/Prefab/SealMaterialManager.cs
@@ -8,7 +8,7 @@ internal class SealMaterialManager : MonoBehaviour, IOnAsyncPrefabTasksCompleted
 {
     public void OnAsyncPrefabTasksCompleted()
     {
-        MaterialUtils.ApplySNShaders(gameObject, 8f, 1.5f, 1, modifiers: new UseDiffuseForSpecular());
+        MaterialUtils.ApplySNShaders(gameObject, 6.5f, 1f, 1, modifiers: new UseDiffuseForSpecular());
 
         foreach (var materialSetter in gameObject.GetComponentsInChildren<MaterialSetter>(true))
         {
