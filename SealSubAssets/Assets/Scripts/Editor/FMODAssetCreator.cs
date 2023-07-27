7adfb37a0b3993ea186088ef76f95988db1e616d
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-27 00:24:18 -0400

    Add FMODAssetCreator window

---
+++ SealSubAssets/Assets/Scripts/Editor/FMODAssetCreator.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class FMODAssetCreator : EditorWindow
+{
+    private string name;
+    private string soundPath;
+    private string soundId;
+
+    [MenuItem("Assets/FMOD Asset Creator")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(FMODAssetCreator));
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Name");
+        name = EditorGUILayout.TextField(name);
+        EditorGUILayout.LabelField("Sound Path");
+        soundPath = EditorGUILayout.TextField(soundPath);
+        EditorGUILayout.LabelField("Sound ID");
+        soundId = EditorGUILayout.TextField(soundId);
+        if (GUILayout.Button("Create"))
+            CreateAsset();
+    }
+    
+    private void CreateAsset()
+    {
+        var asset = CreateInstance<FMODAsset>();
+        asset.name = name;
+        asset.path = soundPath;
+        asset.id = soundId;
+        AssetDatabase.CreateAsset(asset, Path.Combine("Assets", "FMODAssets", $"{name}.asset"));
+    }
+}
