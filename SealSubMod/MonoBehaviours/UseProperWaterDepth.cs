359023d60bb08750389497af3ab3275eef53c39b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-28 11:57:22 -0400

    Fix seal sub not being able to surface

---
+++ SealSubMod/MonoBehaviours/UseProperWaterDepth.cs
@@ -0,0 +1,11 @@
+﻿namespace SealSubMod.MonoBehaviours;
+
+internal class UseProperWaterDepth : MonoBehaviour
+{
+    [SerializeField] float yOffset = 5;
+
+    private void Start()
+    {
+        GetComponent<WorldForces>().waterDepth = Ocean.GetOceanLevel() + yOffset;
+    }
+}
