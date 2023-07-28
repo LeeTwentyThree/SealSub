066a199ff5a55b26843922de2d112efbf09b2525
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-28 00:17:02 -0400

    Add steering wheel model

---
+++ SealSubMod/MonoBehaviours/Prefab/SpawnSteeringWheel.cs
@@ -0,0 +1,16 @@
+﻿using SealSubMod.Interfaces;
+using SealSubMod.Utility;
+
+namespace SealSubMod.MonoBehaviours.Prefab;
+
+internal class SpawnSteeringWheel : MonoBehaviour, IAsyncPrefabSetupOperation
+{
+    public IEnumerator SetupPrefabAsync()
+    {
+        yield return CyclopsReferenceManager.EnsureCyclopsReferenceExists();
+        var model = CyclopsReferenceManager.CyclopsReference.transform.Find("CyclopsMeshAnimated/Submarine_Steering_Console").gameObject;
+        var spawned = Instantiate(model, transform);
+        spawned.transform.localPosition = Vector3.zero;
+        spawned.transform.localEulerAngles = new Vector3(-90, 0, 0);
+    }
+}
