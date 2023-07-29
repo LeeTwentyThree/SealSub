e021454bb807e51383ced567ed980267ef575de3
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-28 20:14:18 -0400

    Component for material edits (ugly thanks to bug)

---
+++ SealSubMod/MonoBehaviours/Prefab/ModifyMaterialProperties.cs
@@ -0,0 +1,75 @@
+﻿using SealSubMod.Interfaces;
+using System;
+
+namespace SealSubMod.MonoBehaviours.Prefab;
+
+internal class ModifyMaterialProperties : MonoBehaviour, ILateMaterialOperation
+{
+    public Renderer renderer;
+    public int[] materialIndices = new[] { 0 };
+
+    // These are for the editor only, they do NOT serialize and will be NULL!
+
+    public ColorProperty[] colorProperties;
+    public FloatProperty[] floatProperties;
+
+    // The actual values should be accessed through these fields
+
+    [SerializeField, HideInInspector] string[] colorPropertyNames;
+    [SerializeField, HideInInspector] Color[] colorPropertyValues;
+
+    [SerializeField, HideInInspector] string[] floatPropertyNames;
+    [SerializeField, HideInInspector] float[] floatPropertyValues;
+
+    // Editor-only operations to work around bugs
+
+    private void OnValidate()
+    {
+        for (int i = 0; i < colorProperties.Length; i++)
+        {
+            colorPropertyNames[i] = colorProperties[i].propertyName;
+            colorPropertyValues[i] = colorProperties[i].color;
+        }
+        for (int i = 0; i < floatProperties.Length; i++)
+        {
+            floatPropertyNames[i] = floatProperties[i].propertyName;
+            floatPropertyValues[i] = floatProperties[i].value;
+        }
+
+        if (renderer == null)
+            renderer = GetComponent<Renderer>();
+    }
+
+    public void OnLateMaterialOperation()
+    {
+        if (!renderer) throw new Exception($"Renderer is null on material setter {name}");
+
+        var mats = renderer.materials; // just setting index doesn't work because you get a different array than the actual one. It's basically passed by value rather than reference
+        foreach (var index in materialIndices)
+        {
+            for (int i = 0; i < colorPropertyNames.Length; i++)
+            {
+                mats[index].SetColor(colorPropertyNames[i], colorPropertyValues[i]);
+            }
+            for (int i = 0; i < floatPropertyNames.Length; i++)
+            {
+                mats[index].SetFloat(floatPropertyNames[i], floatPropertyValues[i]);
+            }
+        }
+        renderer.materials = mats;
+    }
+
+    [Serializable]
+    public class ColorProperty
+    {
+        public string propertyName;
+        public Color color;
+    }
+
+    [Serializable]
+    public class FloatProperty
+    {
+        public string propertyName;
+        public float value;
+    }
+}
