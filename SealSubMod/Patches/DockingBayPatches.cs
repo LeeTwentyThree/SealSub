62cf26dfc7cfc2b4cc88185dc7455d06ee4ed7a8
Nagorrogan <eldritchcarmaker@gmail.com>
2023-07-31 05:56:44 -0500

    Remove commented code

--- SealSubMod/Patches/DockingBayPatches.cs
+++ SealSubMod/Patches/DockingBayPatches.cs
@@ -7,19 +7,6 @@ namespace SealSubMod.Patches;
 [HarmonyPatch(typeof(VehicleDockingBay))]
 internal class DockingBayPatches
 {
-    /*
-    [HarmonyPatch(nameof(VehicleDockingBay.LateUpdate))]
-    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    {
-        var matcher = new CodeMatcher(instructions);
-        matcher.MatchForward(true, new CodeMatch(OpCodes.Stfld, AccessTools.Field(typeof(VehicleDockingBay), nameof(VehicleDockingBay.interpolatingVehicle))));
-        matcher.Insert(new CodeInstruction(OpCodes.Ldarg_0));
-        matcher.Insert(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(VehicleDockingBayPatches), nameof(VehicleDockingBayPatches.IsSealBay))));
-        //OpCodes.ret
-        return matcher.InstructionEnumeration();
-    }
-    public static bool IsSealBay(VehicleDockingBay bay) => bay is SealDockingBay;
-    */
     [HarmonyPatch(nameof(VehicleDockingBay.UpdateDockedPosition))]
     public static bool Prefix(VehicleDockingBay __instance, Vehicle vehicle, float interpfraction)
     {
