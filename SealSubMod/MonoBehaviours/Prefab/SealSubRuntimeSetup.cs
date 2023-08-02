80b4dec18ecd4b19793d8d80f429f9ecbc51ff89
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-02 19:36:16 -0400

    SealSubRuntimeSetup class to assign single values

---
+++ SealSubMod/MonoBehaviours/Prefab/SealSubRuntimeSetup.cs
@@ -0,0 +1,22 @@
+﻿using SealSubMod.Interfaces;
+
+namespace SealSubMod.MonoBehaviours.Prefab;
+
+// Class for assigning component fields at runtime
+internal class SealSubRuntimeSetup : MonoBehaviour, IAsyncPrefabSetupOperation
+{
+    [SerializeField] float waterLevelYOffset = 3;
+
+    // Serialized fields can be set in SetupPrefabAsync
+    public IEnumerator SetupPrefabAsync()
+    {
+        GetComponent<PingInstance>().SetType(Plugin.SealPingType);
+        yield break;
+    }
+
+    // Non-serialized fields must be set in Start
+    private void Start()
+    {
+        GetComponent<WorldForces>().waterDepth = Ocean.GetOceanLevel() + waterLevelYOffset;
+    }
+}
