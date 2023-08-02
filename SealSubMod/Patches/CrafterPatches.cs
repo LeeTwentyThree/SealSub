66b6b6f7075359147c7e2f7e4d7262968ffce60a
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-02 17:41:26 -0400

    Increase craft time of seal sub

---
+++ SealSubMod/Patches/CrafterPatches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using SealSubMod.Prefabs;
+
+namespace SealSubMod.Patches;
+
+[HarmonyPatch(typeof(Crafter))]
+internal class CrafterPatches
+{
+    [HarmonyPatch(nameof(Crafter.Craft))]
+    public static void Prefix(TechType techType, ref float duration)
+    {
+        if (techType == SealSubPrefab.SealType)
+        {
+            duration = 25;
+        }
+    }
+}
