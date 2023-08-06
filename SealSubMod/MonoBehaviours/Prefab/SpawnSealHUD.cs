ee5879c9970d4acefc85a2e279e147767933cdcd
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-06 13:38:32 -0400

    Create class to spawn the HUD

---
+++ SealSubMod/MonoBehaviours/Prefab/SpawnSealHUD.cs
@@ -0,0 +1,22 @@
+﻿using SealSubMod.Interfaces;
+using SealSubMod.Utility;
+
+namespace SealSubMod.MonoBehaviours.Prefab;
+
+internal class SpawnSealHUD : MonoBehaviour, IAsyncPrefabSetupOperation
+{
+    public IEnumerator SetupPrefabAsync()
+    {
+        yield return CyclopsReferenceManager.EnsureCyclopsReferenceExists();
+        var model = CyclopsReferenceManager.CyclopsReference.transform.Find("HelmHUD").gameObject;
+        var spawned = Instantiate(model, transform);
+        spawned.transform.localPosition = Vector3.zero;
+        spawned.transform.localEulerAngles = new Vector3(0, 0, 0);
+        var manager = spawned.GetComponent<CyclopsHelmHUDManager>();
+        var root = transform.parent.gameObject;
+        manager.subRoot = root.GetComponent<SubRoot>();
+        manager.subLiveMixin = root.GetComponent<LiveMixin>();
+        manager.LOD = root.GetComponent<BehaviourLOD>();
+        manager.motorMode = root.GetComponent<CyclopsMotorMode>();
+    }
+}
