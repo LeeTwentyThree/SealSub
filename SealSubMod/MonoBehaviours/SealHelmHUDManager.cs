3ac0d7314ec18b8b51f93dcee62b85a23611231a
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-06 19:05:43 -0400

    Implement prototype of Helm HUD

--- SealSubMod/MonoBehaviours/SealHelmHUDManager.cs
+++ SealSubMod/MonoBehaviours/SealHelmHUDManager.cs
@@ -2,5 +2,66 @@
 
 internal class SealHelmHUDManager : MonoBehaviour
 {
+    [SerializeField] SubRoot subRoot;
+    [SerializeField] LiveMixin subLiveMixin;
+    [SerializeField] CyclopsMotorMode motorMode;
+    [SerializeField] BehaviourLOD LOD;
+    [SerializeField] GameObject hornObject;
+    [SerializeField] CanvasGroup canvasGroup;
 
+    private bool hudActive;
+
+    private void OnValidate()
+    {
+        if (subRoot == null) subRoot = gameObject.GetComponentInParent<SubRoot>();
+        if (subLiveMixin == null) subLiveMixin = gameObject.GetComponentInParent<LiveMixin>();
+        if (motorMode == null) motorMode = gameObject.GetComponentInParent<CyclopsMotorMode>();
+        if (LOD == null) LOD = gameObject.GetComponentInParent<BehaviourLOD>();
+    }
+
+    private void Update()
+    {
+        if (!LOD.IsFull())
+        {
+            return;
+        }
+        if (subLiveMixin.IsAlive())
+        {
+            // Implement health display etc here...
+        }
+        if (Player.main.currentSub == subRoot && !subRoot.subDestroyed)
+        {
+            // Implement other display stuff here...
+
+            if (hudActive)
+            {
+                canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 1f, Time.deltaTime * 3f);
+                canvasGroup.interactable = true;
+            }
+            else
+            {
+                canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 0f, Time.deltaTime * 3f);
+                canvasGroup.interactable = false;
+            }
+        }
+    }
+
+    public void StartPiloting()
+    {
+        hudActive = true;
+        hornObject.SetActive(true);
+    }
+
+    public void StopPiloting()
+    {
+        hudActive = false;
+        hornObject.SetActive(false);
+    }
+
+    public void OnTakeCollisionDamage(float value)
+    {
+        value *= 1.5f;
+        value = Mathf.Clamp(value / 100f, 0.5f, 1.5f);
+        MainCameraControl.main.ShakeCamera(value, -1f, MainCameraControl.ShakeMode.Linear, 1f);
+    }
 }
