7a30418ef65e7dc060f4b9ee02d44ae00977d017
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-07 22:20:58 -0400

    Add animated alarms to seal prefab

---
+++ SealSubMod/MonoBehaviours/AlarmManager.cs
@@ -0,0 +1,16 @@
+﻿namespace SealSubMod.MonoBehaviours;
+
+internal class AlarmManager : MonoBehaviour
+{
+    private AnimateAlarm[] alarms;
+
+    private void Awake()
+    {
+        alarms = GetComponentsInChildren<AnimateAlarm>();
+    }
+
+    public void SetAlarmsEnabled(bool enabled)
+    {
+        alarms.ForEach((a) => a.SetAlarmEnabled(enabled));
+    }
+}
