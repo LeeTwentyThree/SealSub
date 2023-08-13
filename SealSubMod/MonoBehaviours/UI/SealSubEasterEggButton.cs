d96c739983ce31f9425872d1a67a78f4f682160e
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-13 13:59:07 -0400

    Crazy? I was crazy once. They locked me in a room. A rubber room. With rats. Rats make me crazy.

---
+++ SealSubMod/MonoBehaviours/UI/SealSubEasterEggButton.cs
@@ -0,0 +1,11 @@
+﻿namespace SealSubMod.MonoBehaviours.UI;
+
+internal class SealSubEasterEggButton : SealButtonBase
+{
+    [SerializeField] VoiceNotification easterEggVoiceNotification;
+
+    protected override void OnMouseClickBehavior()
+    {
+        subRoot.voiceNotificationManager.PlayVoiceNotification(easterEggVoiceNotification);
+    }
+}
