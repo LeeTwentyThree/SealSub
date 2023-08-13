926871ada14b92f8faccbb83466b47d7847dc1ef
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-13 13:58:20 -0400

    SealButtonBase class

---
+++ SealSubMod/MonoBehaviours/UI/SealButtonBase.cs
@@ -0,0 +1,55 @@
+﻿namespace SealSubMod.MonoBehaviours.UI;
+
+internal class SealButtonBase : MonoBehaviour
+{
+    [HideInInspector] public bool invalidButton;
+
+    public SubRoot subRoot;
+
+    private bool mouseHover;
+
+    private void OnValidate()
+    {
+        if (subRoot == null) subRoot = gameObject.GetComponentInParent<SubRoot>();
+    }
+
+    public void OnMouseEnter()
+    {
+        if (Player.main.currentSub != subRoot)
+        {
+            return;
+        }
+        mouseHover = true;
+        OnMouseEnterBehavior();
+    }
+
+    public void OnMouseExit()
+    {
+        if (Player.main.currentSub != subRoot)
+        {
+            return;
+        }
+        mouseHover = false;
+        OnMouseExitBehavior();
+    }
+
+    public void OnClick()
+    {
+        if (invalidButton)
+        {
+            return;
+        }
+        if (Player.main.currentSub != subRoot)
+        {
+            return;
+        }
+        mouseHover = false;
+        OnMouseClickBehavior();
+    }
+
+    protected virtual void OnMouseClickBehavior() { }
+
+    protected virtual void OnMouseEnterBehavior() { }
+
+    protected virtual void OnMouseExitBehavior() { }
+}
