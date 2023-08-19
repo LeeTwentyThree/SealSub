5aa28f099d1e5a782007757153b45b7fd45345c1
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-19 00:12:02 -0400

    Add EngineUIManager to prefab
    Also added better engine on/off animations to the HUD with bug fixes too

---
+++ SealSubMod/MonoBehaviours/UI/EngineUIManager.cs
@@ -0,0 +1,35 @@
+﻿using SealSubMod.Interfaces;
+
+namespace SealSubMod.MonoBehaviours.UI;
+
+internal class EngineUIManager : MonoBehaviour, IUIElement
+{
+    [SerializeField] CyclopsMotorMode motorMode;
+    [SerializeField] GameObject engineOffIndicator;
+    [SerializeField] Animator toggleSpeedSettingsAnimator;
+
+    public void UpdateUI()
+    {
+        engineOffIndicator.SetActive(!motorMode.engineOn);
+    }
+
+    public void StartPiloting()
+    {
+        if (motorMode.engineOn)
+        {
+            toggleSpeedSettingsAnimator.SetTrigger("EngineOn");
+            return;
+        }
+        toggleSpeedSettingsAnimator.SetTrigger("EngineOff");
+    }
+
+    public void InvokeChangeEngineState(bool changeToState)
+    {
+        if (changeToState)
+        {
+            toggleSpeedSettingsAnimator.SetTrigger("StartEngine");
+            return;
+        }
+        toggleSpeedSettingsAnimator.SetTrigger("StopEngine");
+    }
+}
