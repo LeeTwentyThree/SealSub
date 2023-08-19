8d6ac13f65c461b5a8cdf954594b77e94f1737b9
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-19 00:26:23 -0400

    Fix child object identifier Class IDs changing

---
+++ SealSubMod/MonoBehaviours/Prefab/ChildObjectIdentifierFix.cs
@@ -0,0 +1,25 @@
+﻿using SealSubMod.Interfaces;
+
+namespace SealSubMod.MonoBehaviours.Prefab;
+
+// If a child object identifier is not assigned a consistent Class ID, its contents will be reset with every session!!!
+
+internal class ChildObjectIdentifierFix : MonoBehaviour, IAsyncPrefabSetupOperation
+{
+    public string classId;
+    public ChildObjectIdentifier childObjectIdentifier;
+
+    public IEnumerator SetupPrefabAsync(GameObject prefabRoot)
+    {
+        childObjectIdentifier.ClassId = classId;
+        yield break;
+    }
+
+    private void OnValidate()
+    {
+        if (childObjectIdentifier == null)
+        {
+            childObjectIdentifier = GetComponent<ChildObjectIdentifier>();
+        }
+    }
+}
