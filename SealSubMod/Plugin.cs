2974eae850ead4ea2e42f6ccdd59c29967074996
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-19 18:40:14 -0400

    Add depth modules (nonfunctional)

--- SealSubMod/Plugin.cs
+++ SealSubMod/Plugin.cs
@@ -1,6 +1,10 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Assets.PrefabTemplates;
+using Nautilus.Crafting;
 using SealSubMod.Commands;
 using SealSubMod.MonoBehaviours.Prefab;
 using SealSubMod.Prefabs;
@@ -22,6 +26,15 @@ public class Plugin : BaseUnityPlugin
 
     internal static PingType SealPingType { get; private set; }
 
+    internal static PrefabInfo DepthModuleMk1Info { get; } = PrefabInfo.WithTechType("SealHullModule1", null, null)
+        .WithIcon(SpriteManager.Get(TechType.CyclopsHullModule1));
+
+    internal static PrefabInfo DepthModuleMk2Info { get; } = PrefabInfo.WithTechType("SealHullModule2", null, null)
+    .WithIcon(SpriteManager.Get(TechType.CyclopsHullModule2));
+
+    internal static PrefabInfo DepthModuleMk3Info { get; } = PrefabInfo.WithTechType("SealHullModule3", null, null)
+    .WithIcon(SpriteManager.Get(TechType.CyclopsHullModule3));
+
     private void Awake()
     {
         // set project-scoped logger instance
@@ -31,8 +44,6 @@ public class Plugin : BaseUnityPlugin
         Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
 
         ConsoleCommandsHandler.RegisterConsoleCommands(typeof(ConsoleCommands));
-
-        SealSubPrefab.Register();
 
         UWE.CoroutineHost.StartCoroutine(MaterialSetter.LoadMaterialsAsync());
 
@@ -46,8 +57,29 @@ public class Plugin : BaseUnityPlugin
             Equipment.slotMapping.Add($"SealModule{i}", SealModuleEquipmentType);
         }
 
+        RegisterPrefabs();
+
         ModAudio.RegisterAudio(assets);
 
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void RegisterPrefabs()
+    {
+        SealSubPrefab.Register();
+
+        RegisterUpgradeModulePrefab(DepthModuleMk1Info, new RecipeData(new CraftData.Ingredient(TechType.Titanium, 2)));
+        RegisterUpgradeModulePrefab(DepthModuleMk2Info, new RecipeData(new CraftData.Ingredient(TechType.Titanium, 2)));
+        RegisterUpgradeModulePrefab(DepthModuleMk3Info, new RecipeData(new CraftData.Ingredient(TechType.Titanium, 2)));
+    }
+
+    private static void RegisterUpgradeModulePrefab(PrefabInfo info, RecipeData recipe)
+    {
+        var prefab = new CustomPrefab(info);
+        prefab.SetGameObject(new CloneTemplate(info, TechType.CyclopsHullModule1));
+        prefab.SetPdaGroupCategory(TechGroup.VehicleUpgrades, TechCategory.VehicleUpgrades);
+        prefab.SetRecipe(recipe);
+        prefab.SetEquipment(SealModuleEquipmentType);
+        prefab.Register();
     }
 }
