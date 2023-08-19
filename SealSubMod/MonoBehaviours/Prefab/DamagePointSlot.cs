c363ac80b32bb0b1965697b50e1d85e399c0386b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-19 16:49:12 -0400

    Refactor damage point spawning, for no reason ig

---
+++ SealSubMod/MonoBehaviours/Prefab/DamagePointSlot.cs
@@ -0,0 +1,10 @@
+﻿namespace SealSubMod.MonoBehaviours.Prefab;
+
+internal class DamagePointSlot : MonoBehaviour
+{
+    //idfk how to do paragraphs
+    [Header("The specific child index of cyclops damage")]
+    [Header("prefabs to use for this object")]
+    [Header("use -1 to pick a random one")]
+    public int damagePrefabIndex = -1;
+}
--- SealSubMod/MonoBehaviours/Prefab/SpawnCyclopsDamagePoints.cs
+++ SealSubMod/MonoBehaviours/Prefab/SpawnCyclopsDamagePoints.cs
@@ -5,15 +5,9 @@ using SealSubMod.Utility;
 
 namespace SealSubMod.MonoBehaviours.Prefab;
 
-internal class SpawnCyclopsDamagePoint : PrefabModifierAsync
+internal class SpawnCyclopsDamagePoints : PrefabModifierAsync
 {
     private static CyclopsExternalDamageManager cyclopsManager;
-
-    //idfk how to do paragraphs
-    [Header("The specific child index of cyclops damage")]
-    [Header("prefabs to use for this object")]
-    [Header("use -1 to pick a random one")]
-    [SerializeField] int damagePrefabIndex = -1;
 
     [SerializeField] CyclopsExternalDamageManager damageManager;
 
@@ -21,13 +15,18 @@ internal class SpawnCyclopsDamagePoint : PrefabModifierAsync
     {
         if (!cyclopsManager) yield return LoadCyclopsManager();
 
-        if (damagePrefabIndex <= -1) damagePrefabIndex = Random.Range(0, cyclopsManager.damagePoints.Length);
+        var points = new List<CyclopsDamagePoint>();
 
-        var prefab = cyclopsManager.damagePoints[damagePrefabIndex].gameObject;
-        var copy = Instantiate(prefab, transform.position, transform.rotation, transform);
+        foreach (var slot in gameObject.GetComponentsInChildren<DamagePointSlot>())
+        {
+            if (slot.damagePrefabIndex <= -1) slot.damagePrefabIndex = Random.Range(0, cyclopsManager.damagePoints.Length);
 
-        var points = damageManager.damagePoints.ToList();
-        points.Add(copy.GetComponent<CyclopsDamagePoint>());
+            var prefab = cyclopsManager.damagePoints[slot.damagePrefabIndex].gameObject;
+            var copy = Instantiate(prefab, transform.position, transform.rotation, transform);
+
+            points.Add(copy.GetComponent<CyclopsDamagePoint>());
+        }
+
         damageManager.damagePoints = points.ToArray();
     }
 
