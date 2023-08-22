ca3fbad8cf89e14744bed6a588fca01f7aff3666
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-21 21:20:11 -0400

    Create CameraDefaultOrientationFix.cs

---
+++ SealSubMod/MonoBehaviours/Prefab/CameraDefaultOrientationFix.cs
@@ -0,0 +1,17 @@
+﻿namespace SealSubMod.MonoBehaviours.Prefab;
+
+internal class CameraDefaultOrientationFix : MonoBehaviour
+{
+    public Transform parent;
+
+    public Vector3 angle;
+
+    private void OnValidate()
+    {
+        if (parent)
+        {
+            transform.forward = parent.forward;
+            transform.eulerAngles += angle;
+        }
+    }
+}
