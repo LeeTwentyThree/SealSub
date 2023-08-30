33d51c8ec6e4e65f6eb7ce585b7d61f04b378313
Nagorrogan <eldritchcarmaker@gmail.com>
2023-08-30 01:36:08 -0500

    Update CyclopsExternalDamageManagerPatches.cs

--- SealSubMod/Patches/CyclopsExternalDamageManagerPatches.cs
+++ SealSubMod/Patches/CyclopsExternalDamageManagerPatches.cs
@@ -7,5 +7,11 @@ namespace SealSubMod.Patches;
 internal class CyclopsExternalDamageManagerPatches
 {
     [HarmonyPatch(nameof(CyclopsExternalDamageManager.OnEnable))]
-    public static bool Prefix(CyclopsExternalDamageManager __instance) => __instance.subRoot is not SealSubRoot;
+    public static bool Prefix(CyclopsExternalDamageManager __instance) => __instance.subRoot is not SealSubRoot;//lee we need to remove this
+    //I'm lazy
+    //it just uses the sub fire
+    //which we don't set
+    //and the subfire class sucks
+    //so we don't want to set it
+    //transpiler shouldn't be hard but I'm tired rn
 }
