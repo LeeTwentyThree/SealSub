9cc43ab2faafb837af650829e2c0288c811086fd
Nagorrogan <eldritchcarmaker@gmail.com>
2023-11-20 15:28:12 -0600

    Mostly ready map room functionality shit
    Still few things to go, plus model changes, but its like okay as is

---
+++ SealSubMod/MonoBehaviours/Prefab/MapRoomFunctionalitySpawner.cs
@@ -0,0 +1,42 @@
+﻿using SealSubMod.MonoBehaviours.Abstract;
+
+namespace SealSubMod.MonoBehaviours.Prefab;
+
+internal class MapRoomFunctionalitySpawner : MonoBehaviour
+{
+    [SerializeField] Transform fabricatorPosition;
+
+    private IEnumerator Start()
+    {
+        if (!Base.mapRoomFunctionalityPrefab)
+        {
+            Base.Initialize();
+            yield return new WaitUntil(() => Base.mapRoomFunctionalityPrefab);
+        }
+        var func = Instantiate(Base.mapRoomFunctionalityPrefab, transform.position, transform.rotation, transform);
+
+        foreach (Transform child in func.transform)
+        {
+            if (child.name.Contains("dockingPoint"))
+                Destroy(child.gameObject);
+        }
+        func.GetComponent<MapRoomFunctionality>().miniWorld.updatePosition = true;
+
+        Destroy(func.transform.Find("hologram/worlddisplay/MapRoomFX/x_MapRoom_HoloTableGlow_Top"));
+
+        if(fabricatorPosition)
+        {
+            var fabSpawn = func.GetComponentInChildren<PrefabSpawn>(true);
+            if (!fabSpawn) yield break;
+            var upgrades = func.GetComponentInChildren<StorageContainer>(true);
+            var upgradesOffset = upgrades.transform.position - fabSpawn.transform.position;
+            fabSpawn.transform.parent = fabricatorPosition;
+            fabSpawn.transform.position = fabricatorPosition.position;
+            fabSpawn.transform.rotation = fabricatorPosition.rotation;
+
+            upgrades.transform.parent = fabricatorPosition;
+            upgrades.transform.position = fabricatorPosition.position + upgradesOffset;
+            upgrades.transform.parent.rotation = fabricatorPosition.rotation;
+        }
+    }
+}
---
+++ SealSubMod/Patches/MapRoomFunctionalityPatches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection.Emit;
+
+namespace SealSubMod.Patches;
+
+[HarmonyPatch(typeof(MapRoomFunctionality))]
+internal class MapRoomFunctionalityPatches
+{
+    [HarmonyPatch(nameof(MapRoomFunctionality.Start))]
+    public static IEnumerable<CodeInstruction> Transpiler/*? I hardly kn-*/(IEnumerable<CodeInstruction> instructions)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        var meth = AccessTools.Method(typeof(Component), nameof(Component.GetComponentInParent), Array.Empty<Type>(), new Type[1] { typeof(Base) });
+        matcher.MatchForward(false, new CodeMatch(OpCodes.Ldarg_0), new CodeMatch(OpCodes.Call, meth));
+
+        matcher.RemoveInstructionsInRange(matcher.Pos + 1, matcher.Pos + 5);
+        matcher.Advance(1);
+        matcher.Insert(Transpilers.EmitDelegate<Action<MapRoomFunctionality>>((functionality) =>
+        {
+            if (functionality.GetComponentInParent<Base>())//original method didn't have a safety check here, it assumed it would always find a base, and threw an error
+                functionality.GetComponentInParent<Base>().onPostRebuildGeometry += functionality.OnPostRebuildGeometry;
+        }));
+
+        return matcher.InstructionEnumeration();
+    }
+}
