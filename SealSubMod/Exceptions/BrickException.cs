61b925733c782bc7a5cda3fa1396253090791ee6
Nagorrogan <eldritchcarmaker@gmail.com>
2024-01-02 10:30:24 -0600

    Create BrickException.cs

---
+++ SealSubMod/Exceptions/BrickException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SealSubMod.Exceptions;
+
+internal class Brick : Exception
+{
+    public Brick()
+    {
+        throw new NotImplementedException("Why is there a brick here");
+    }
+}
