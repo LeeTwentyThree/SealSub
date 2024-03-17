2ca04fbe59b4c77a1fdc3f1c0196a30ac1839f86
Nagorrogan <eldritchcarmaker@gmail.com>
2024-03-17 06:44:42 -0500

    doors

---
+++ SealSubMod/MonoBehaviours/SlidingDoor.cs
@@ -0,0 +1,34 @@
+﻿namespace SealSubMod.MonoBehaviours;
+
+internal class SlidingDoor : MonoBehaviour
+{
+    [SerializeField] Vector3 openPos;
+    private Vector3 defaultLocalPos;
+    private float speed = 1;
+    private float state = 0;
+    private float target = 0;
+    public void SetOpen(bool open)
+    {
+        target = open ? 1 : 0;
+    }
+    public void Toggle()
+    {
+        target = Mathf.Approximately(target, 0) ? 1 : 0;
+    }
+
+    private void Awake()
+    {
+        defaultLocalPos = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        var direction = Mathf.Sign(target - state);
+        if (direction == 0) return;
+
+        state += speed * direction * Time.deltaTime;
+        state = Mathf.Clamp01(state);
+
+        transform.localPosition = defaultLocalPos + Vector3.Lerp(Vector3.zero, openPos, state);
+    }
+}
