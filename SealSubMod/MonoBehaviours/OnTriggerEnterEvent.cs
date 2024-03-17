f702a77f3778988836366fd59dfa9f31b00cd784
Nagorrogan <eldritchcarmaker@gmail.com>
2024-03-17 07:53:02 -0500

    Sliding door triggers
    Sorry lee I had to remove the alarms in order to build the mod properly because unity still isn't working with .blend files but once they're converted to fbx should be able to throw them back in

---
+++ SealSubMod/MonoBehaviours/OnTriggerEnterEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.Events;
+
+namespace SealSubMod.MonoBehaviours;
+
+internal class OnTriggerEnterEvent : MonoBehaviour
+{
+    public UnityEvent onTriggerEnter;
+    public UnityEvent onTriggerExit;
+    public UnityEvent onTriggerStay;
+    private void OnTriggerEnter(Collider collider)
+    {
+        onTriggerEnter.Invoke();
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        onTriggerExit.Invoke();
+    }
+    private void OnTriggerStay(Collider collider)
+    {
+        onTriggerStay.Invoke();
+    }
+}
