1cebc7e5ae737f7ec03a479809884d330876f99e
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-03-22 23:54:01 -0400

    Implement the basics of flooding

---
+++ SealSubMod/MaterialModifiers/BlockShaderConversions.cs
@@ -0,0 +1,16 @@
+﻿using Nautilus.Utility.MaterialModifiers;
+
+namespace SealSubMod.MaterialModifiers;
+
+internal class BlockShaderConversions : MaterialModifier
+{
+    public override void EditMaterial(Material material, Renderer renderer, int materialIndex, MaterialUtils.MaterialType materialType)
+    {
+
+    }
+    public override bool BlockShaderConversion(Material material, Renderer renderer, MaterialUtils.MaterialType materialType)
+    {
+        //yea that typo is intentional
+        return renderer.name.Contains("Volumletric") || renderer.name.Contains("x_BaseWater");
+    }
+}
---
+++ SealSubMod/MonoBehaviours/Prefab/SealFloodPlaneFixer.cs
@@ -0,0 +1,38 @@
+﻿using SealSubMod.MonoBehaviours.Abstract;
+using UWE;
+
+namespace SealSubMod.MonoBehaviours.Prefab;
+
+public class SealFloodPlaneFixer : PrefabModifierAsync
+{
+    public BaseWaterPlane waterPlane;
+
+    public override IEnumerator SetupPrefabAsync()
+    {
+        var request = PrefabDatabase.GetPrefabForFilenameAsync("Assets/Prefabs/Base/GeneratorPieces/BaseRoom.prefab");
+        yield return request;
+        if (!request.TryGetPrefab(out var baseRoomPrefab))
+        {
+            Plugin.Logger.LogError("Failed to load the multipurpose room prefab!");
+            yield break;
+        }
+
+        var referenceWaterPlane = baseRoomPrefab.transform.Find("Flood_BaseRoom").GetComponent<BaseWaterPlane>();
+        var planeMesh = Instantiate(referenceWaterPlane.transform.Find("x_BaseWaterPlane_RoomGen").gameObject,
+            waterPlane.transform);
+        planeMesh.transform.localPosition = Vector3.zero;
+        var fogMesh = Instantiate(referenceWaterPlane.transform.Find("x_BaseWaterFog_BaseRoom").gameObject,
+            waterPlane.transform);
+        fogMesh.transform.localPosition = Vector3.zero;
+        waterPlane.waterPlane = planeMesh.transform;
+        waterPlane.waterRender = planeMesh.GetComponent<Renderer>();
+        waterPlane.fogRenderers = new[] {fogMesh.GetComponent<Renderer>()};
+        var childTransforms = planeMesh.GetComponentsInChildren<Transform>();
+        var children = new GameObject[childTransforms.Length];
+        for (int i = 0; i < childTransforms.Length; i++)
+        {
+            children[i] = childTransforms[i].gameObject;
+        }
+        waterPlane.children = children;
+    }
+}
